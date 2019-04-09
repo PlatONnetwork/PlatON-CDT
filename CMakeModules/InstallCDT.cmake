8cbc890e8b1507a19490d3a0a2bd6f98d05e6025
mowind <wjinwen.1988@gmail.com>
2019-04-09 15:09:04 +0800

    added external dependencies projects

---
+++ CMakeModules/InstallCDT.cmake
@@ -0,0 +1,37 @@
+add_custom_command(TARGET PlatonLLVM POST_BUILD COMMAND mkdir -p ${CMAKE_BINARY_DIR}/bin)
+macro(platon_llvm_install file)
+  set(BINARY_DIR ${CMAKE_BINARY_DIR}/platon_llvm/bin)
+  add_custom_command(TARGET PlatonLLVM POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy ${BINARY_DIR}/${file} ${CMAKE_BINARY_DIR}/bin/)
+  install(FILES ${BINARY_DIR}/${file}
+    DESTINATION ${CMAKE_INSTALL_FULL_BINDIR}
+    PERMISSIONS OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+endmacro(platon_llvm_install)
+
+macro(platon_tools_install file)
+  set(BINARY_DIR ${CMAKE_BINARY_DIR}/tools/bin)
+  add_custom_command(TARGET PlatonTools POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy ${BINARY_DIR}/${file} ${CMAKE_BINARY_DIR}/bin/)
+  install(FILES ${BINARY_DIR}/${file}
+    DESTINATION ${CMAKE_INSTALL_FULL_BINDIR}
+    PERMISSIONS OWNER_READ OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE)
+endmacro(platon_tools_install)
+
+macro(platon_libraries_install)
+   execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/lib)
+   execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/include)
+   install(DIRECTORY ${CMAKE_BINARY_DIR}/lib/ DESTINATION ${CMAKE_INSTALL_PREFIX}/lib)
+   install(DIRECTORY ${CMAKE_BINARY_DIR}/include/ DESTINATION ${CMAKE_INSTALL_FULL_INCLUDEDIR})
+   install(DIRECTORY ${CMAKE_SOURCE_DIR}/libraries/boost/include/ DESTINATION ${CMAKE_INSTALL_FULL_INCLUDEDIR})
+   install(DIRECTORY ${CMAKE_SOURCE_DIR}/libraries/rapidjson/include/ DESTINATION ${CMAKE_INSTALL_FULL_INCLUDEDIR})
+   install(DIRECTORY ${CMAKE_SOURCE_DIR}/libraries/platonlib/include/ DESTINATION ${CMAKE_INSTALL_FULL_INCLUDEDIR})
+endmacro(platon_libraries_install)
+
+platon_llvm_install(clang-7)
+platon_llvm_install(opt)
+platon_llvm_install(llc)
+platon_llvm_install(lld)
+platon_llvm_install(wasm-ld)
+platon_tools_install(platon-cpp)
+platon_tools_install(platon-ld)
+platon_tools_install(platon-init)
+platon_tools_install(platon-abigen)
+platon_libraries_install()
