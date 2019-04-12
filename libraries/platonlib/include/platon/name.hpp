4cb24a1944915133d61be5e8ddaa17d703bc10cc
mowind <wjinwen.1988@gmail.com>
2019-04-12 14:47:16 +0800

    fix operator ""_n

--- libraries/platonlib/include/platon/name.hpp
+++ libraries/platonlib/include/platon/name.hpp
@@ -3,8 +3,6 @@
 
 #include <string>
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wgnu-string-literal-operator-template"
 
 namespace platon {
 namespace detail {
@@ -20,10 +18,13 @@ struct to_const_char_arr {
  *
  * @brief "foo"_n representing a constant of type `const char*` and value is "foo"
  */
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wgnu-string-literal-operator-template"
 template <typename T, T... Str>
 inline constexpr const char* operator""_n() {
   constexpr const char* ss = platon::detail::to_const_char_arr<Str...>::value;
   return ss;
 }
+#pragma clang diagnostic pop
 
 #endif  // __NAME_H_
