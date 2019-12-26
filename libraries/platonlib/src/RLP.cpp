23360fe13c267e424adc0f2b601d7b2b47b0f93c
wanghengtao <wanghengtao@juzix.net>
2019-12-26 19:41:18 +0800

    Change the storage implementation to RLP.

--- libraries/platonlib/src/RLP.cpp
+++ libraries/platonlib/src/RLP.cpp
@@ -1,28 +1,10 @@
-/*
-	This file is part of cpp-ethereum.
-
-	cpp-ethereum is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	cpp-ethereum is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with cpp-ethereum.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/*
 #include "platon/RLP.h"
+using namespace std;
 
 namespace platon {
+
 bytes RLPNull = rlp("");
 bytes RLPEmptyList = rlpList();
-
-
 
 RLP::RLP(bytesConstRef _d, Strictness _s):
         m_data(_d)
@@ -331,7 +313,6 @@ void RLPStream::pushCount(size_t _count, byte _base)
     pushInt(_count, br);
 }
 }
-*/
 
 //static void streamOut(std::ostream& _out, dev::RLP const& _d, unsigned _depth = 0)
 //{
