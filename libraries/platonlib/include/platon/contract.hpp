72bc3344fba0c0035d9adb389133ea24650edc3e
wanghengtao <wanghengtao@juzix.net>
2019-12-26 19:35:44 +0800

    Added contract related macros ACTION and CONST and CONTRACT

--- libraries/platonlib/include/platon/contract.hpp
+++ libraries/platonlib/include/platon/contract.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
 #define PLATON_ABI(NAME, MEMBER)
-
-
+#define ACTION  __attribute__((annotate("action")))
+#define CONST  __attribute__((annotate("const")))
+#define CONTRACT class 
 
 namespace platon {
     class Contract {
