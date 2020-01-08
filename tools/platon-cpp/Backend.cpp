5d991f51fab6e5a84f7eb9475b02ceaa6fd8264a
wang <wangjiaxing@juzix.net>
2020-01-08 19:08:14 +0800

    close thread of wasm-ld

--- tools/platon-cpp/Backend.cpp
+++ tools/platon-cpp/Backend.cpp
@@ -298,6 +298,7 @@ int GenerateWASM(PCCOption &Option, llvm::Module* M){
     lldArgs.push_back(Option.ldArgs[i].data());
   }
 
+  lldArgs.push_back("--no-threads");
   lldArgs.push_back("--entry");
   lldArgs.push_back("invoke");
   lldArgs.push_back("-o");
