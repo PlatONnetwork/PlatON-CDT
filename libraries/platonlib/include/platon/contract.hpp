82a17804733b1c090aad80991743347539aaccdd
jatel <jatelmomo@hotmail.com>
2020-01-10 15:47:10 +0800

    Add the used property to avoid being optimized away by the compiler

--- libraries/platonlib/include/platon/contract.hpp
+++ libraries/platonlib/include/platon/contract.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #define PLATON_ABI(NAME, MEMBER)
-#define ACTION  __attribute__((annotate("Action")))
-#define CONST  __attribute__((annotate("Const")))
+#define ACTION  __attribute__((used, annotate("Action")))
+#define CONST  __attribute__((used, annotate("Const")))
 #define CONTRACT class 
 
 namespace platon {
