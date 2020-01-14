052c1dd8c8c3590fb022385bb35420b8a05eaa82
jatel <jatelmomo@hotmail.com>
2020-01-14 11:17:39 +0800

    Modify the pointer type to add the appropriate unit test.

--- libraries/platonlib/test/cross_call_test.cpp
+++ libraries/platonlib/test/cross_call_test.cpp
@@ -1,6 +1,8 @@
 #include "platon/cross_call.hpp"
 #include <iostream>
 
+platon::bytes global_bytes;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -16,6 +18,17 @@ void platon_panic(){
     std::cout << "platon panic"<< std::endl;
 }
 
+size_t platon_get_call_output_length(){
+    return global_bytes.size();
+}
+
+void platon_get_call_output(uint8_t *value){
+    for (auto one :global_bytes) {
+        *value = one;
+        value++;
+    }
+}
+
 #ifdef __cplusplus
 }
 #endif
@@ -25,10 +38,15 @@ int main(int argc, char **argv) {
     std::tuple<std::string, std::string, uint32_t> call_paras;
     platon::fetch(platon::RLP(paras), call_paras);
     std::cout << std::get<0>(call_paras) << ' ' << std::get<1>(call_paras) << ' ' << std::get<2>(call_paras) << std::endl;
+    
     uint32_t value = 4 + 256*3 + 256*256*2 + 256*256*256*1;
     platon::bytes value_byte = platon::value_to_bytes(value);
     for(auto one_byte : value_byte){
         std::cout << uint32_t(one_byte) << std::endl;
     }
 
+    global_bytes = paras;
+    std::tuple<std::string, std::string, uint32_t> call_result;
+    platon::get_call_output(call_result);
+    std::cout << std::get<0>(call_result) << ' ' << std::get<1>(call_result) << ' ' << std::get<2>(call_result) << std::endl;
 }
