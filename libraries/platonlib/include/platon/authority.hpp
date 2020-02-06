096285bb7cb68ff84149f72d43cf6ac0a22ab7a6
wangjinwen <wangjinwen@juzix.net>
2020-02-06 15:31:54 +0800

    added comments

--- libraries/platonlib/include/platon/authority.hpp
+++ libraries/platonlib/include/platon/authority.hpp
@@ -20,6 +20,11 @@ static const size_t address_len = 20;
 static const char sys_whitelist_name[] = "$platon$whitelist";
 static const char sys_owner_name[] = "$platon$owner";
 
+/**
+ * @brief Get the address of the transaction initiator
+ *
+ * @return The address of the transaction initiator
+ */
 Address platon_caller() {
   bytes address_bytes;
   address_bytes.resize(address_len);
@@ -27,6 +32,11 @@ Address platon_caller() {
   return Address(address_bytes);
 }
 
+/**
+ * @brief Get the address of the original transaction initiator
+ *
+ * @return The address of the original transaction initiator
+ */
 Address platon_origin_caller() {
   bytes address_bytes;
   address_bytes.resize(address_len);
@@ -34,6 +44,11 @@ Address platon_origin_caller() {
   return Address(address_bytes);
 }
 
+/**
+ * @brief Get the address of contract
+ *
+ * @return The address of contract
+ */
 Address platon_contract_address() {
   bytes address_bytes;
   address_bytes.resize(address_len);
@@ -41,6 +56,12 @@ Address platon_contract_address() {
   return Address(address_bytes);
 }
 
+/**
+ * @brief Set the address as contracts owner
+ *
+ * @param address Accounts address, if address empty that
+ *                set caller as contract owner
+ */
 void set_owner(const std::string &address = std::string()) {
   Address platon_addr(address);
   if (address.empty()) {
@@ -51,11 +72,23 @@ void set_owner(const std::string &address = std::string()) {
   owner_addr.self() = platon_addr;
 }
 
+/**
+ * @brief Get the owner of contact
+ *
+ * @return The address of contract
+ */
 Address owner() {
   StorageType<sys_owner_name, Address> owner_addr;
   return owner_addr.self();
 }
 
+/**
+ * @brief Whether the input address is owner of contract
+ *
+ * @param addr Accounts address, if `addr` empty that set
+ *             `addr` to origin caller
+ * @return true if the input address is owner, false otherwise
+ */
 bool is_owner(const std::string &addr = std::string()) {
   Address platon_addr(addr);
   if (addr.empty()) {
@@ -66,18 +99,61 @@ bool is_owner(const std::string &addr = std::string()) {
   return owner_addr.self() == platon_addr;
 }
 
+/**
+ * @brief Persist storage whitelist implement
+ *
+ * @tparam Name Whitelist name, in the same contract, the name should be unique
+ */
 template <const char *Name>
 class WhiteList {
  public:
+  /**
+   * @brief Construct a new whitlist.
+   */
   WhiteList() : whitelist_() {}
 
+  /**
+   * @brief Add the address to whitelist
+   *
+   * @param addr Accounts address
+   */
   void Add(const std::string &addr) { Add(Address(addr)); }
+
+  /**
+   * @brief Add the address to whitelist
+   *
+   * @param addr Accounts address
+   */
   void Add(const Address &addr) { whitelist_.self().insert(addr); }
 
+  /**
+   * @brief Delete the address from whitelist
+   *
+   * @param addr Accounts address
+   */
   void Delete(const std::string &addr) { Delete(Address(addr)); }
+
+  /**
+   * @brief Delete the address from whitelist
+   *
+   * @param addr Accounts address
+   */
   void Delete(const Address &addr) { whitelist_.self().erase(addr); }
 
+  /**
+   * @brief Whether the address exists in whitelist
+   *
+   * @param addr Accounts address
+   * @return true if exists, false otherwise
+   */
   bool Exists(const std::string &addr) { return Exists(Address(addr)); }
+
+  /**
+   * @brief Whether the address exists in whitelist
+   *
+   * @param addr Accounts address
+   * @return true if exists, false otherwise
+   */
   bool Exists(const Address &addr) {
     return whitelist_.self().find(addr) != whitelist_.self().end();
   }
@@ -86,6 +162,9 @@ class WhiteList {
   Set<Name, Address> whitelist_;
 };
 
+/**
+ * @brief System default whitelist
+ */
 using SysWhitelist = WhiteList<sys_whitelist_name>;
 
 }  // namespace platon
