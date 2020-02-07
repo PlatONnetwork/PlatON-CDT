961775c29798433141e0ebfc23ecdf89d2dfd090
wangjinwen <wangjinwen@juzix.net>
2020-02-07 11:40:47 +0800

    add more operator and more currency unit

--- libraries/platonlib/include/platon/exchange.hpp
+++ libraries/platonlib/include/platon/exchange.hpp
@@ -11,13 +11,35 @@ namespace platon {
  * @brief The unit of Energon
  */
 enum EnergonUnit {
-  VonUnit = 1,
-  GVonUnit = 1000000000,
-  LATUnit = 1000000000000000000
+  VON = 1,
+  kVON = 1000,
+  mVON = 1000000,
+  gVON = 1000000000,
+  uLAT = 1000000000000,
+  mLAT = 1000000000000000,
+  LAT = 1000000000000000000
 };
 
 /**
  * @brief Energo is a type of currency
+ *
+ * Example:
+ *   void test() {
+ *     // New a Energon
+ *     Energon e(10);
+ *     u256 von = 10;
+ *     Energon o(u256);
+ *
+ *     // Gets value
+ *     u256 val = e.Get();
+ *
+ *     // Serialize to big-ending bytes
+ *     bytes bs = e.Bytes();
+ *
+ *     // Add amount of Energon
+ *     Energon sum = e.Add(0);
+ *     e += o;
+ *   }
  */
 class Energon {
  public:
@@ -94,33 +116,70 @@ class Energon {
 /**
  * string-literal operator
  *
- * @brief 1_Von representing one Von
- * @param von Amount of Von
+ * @brief 1_VON representing one VON
+ *
+ * @param von Amount of VON
  * @return Energon
+ *
+ * Energon e = 100_VON;
  */
-Energon operator""_Von(uint64_t von) { return Energon(von); }
+Energon operator""_VON(uint64_t von) { return Energon(von); }
 
 /**
  * string-literal operator
  *
- * @breif 1_GVon representing 1e9Von
- * @param gvon Amount of GVon
+ * @brief 1_kVON representing 1000 VON
+ *
+ * @param kvon Amount of kVON
  * @return Energon
+ *
+ * Energon e = 10_kVON;
  */
-Energon operator""_GVon(long double gvon) {
-  return Energon(gvon * u256(GVonUnit));
-}
+Energon operator""_kVON(uint64_t kvon) { return Energon(kvon * u256(kVON)); }
+
+/**
+ * @brief 1_mVON representing 1e3 VON
+ *
+ * @param mvon Amount of mVON
+ * @return Energon
+ *
+ * Energon e = 5_mVON;
+ */
+Energon operator""_mVON(uint64_t mvon) { return Energon(mvon * u256(mVON)); }
 
 /**
  * string-literal operator
  *
- * @breif 1_GVon representing 1e9Von
- * @param gvon Amount of GVon
+ * @brief 1_GVon representing 1e9 VON
+ *
+ * @param gvon Amount of gVON
  * @return Energon
+ *
+ * Energon e = 2.1111_gVON;
  */
-Energon operator""_GVon(uint64_t gvon) {
-  return Energon(gvon * u256(GVonUnit));
-}
+Energon operator""_gVON(uint64_t gvon) { return Energon(gvon * u256(gVON)); }
+
+/**
+ * string-literal operator
+ *
+ * @brief 1_uLAT representing 1e12 VON
+ * @param ulat Amount of uLAT
+ * @return Energon
+ *
+ * Energon e = 1_uLAT;
+ */
+Energon operator""_uLAT(uint64_t ulat) { return Energon(ulat * u256(uLAT)); }
+
+/**
+ * string-literal operator
+ *
+ * @brief 1_mLAT representing 1e15 VON
+ * @param mlat Amount of mLAT
+ * @return Energon
+ *
+ * Energon e = 6_mLAT;
+ */
+Energon operator""_mLAT(uint64_t mlat) { return Energon(mlat * u256(mLAT)); }
 
 /**
  * string-literal operator
@@ -128,27 +187,86 @@ Energon operator""_GVon(uint64_t gvon) {
  * @brief 1_LAT representing 1e18Von
  * @param lat Amount of LAT
  * @return Energon
+ *
+ * Energon e = 33_LAT;
  */
-Energon operator""_LAT(long double lat) { return Energon(lat * u256(LATUnit)); }
+Energon operator""_LAT(uint64_t lat) { return Energon(lat * u256(LAT)); }
 
 /**
- * string-literal operator
+ * @brief Compare two optional objects lhs and rhs is equal
  *
- * @brief 1_LAT representing 1e18Von
- * @param lat Amount of LAT
- * @return Energon
+ * @param lhs Amount of VON
+ * @param rhs Amount of VON
+ * @return true if lhs == rhs, false otherwise
+ *
+ * Energon lhs(10);
+ * Energon rhs(10);
+ * if (lhs == rhs) {}
  */
-Energon operator""_LAT(uint64_t lat) { return Energon(lat * u256(LATUnit)); }
+bool operator==(const Energon& lhs, const Energon& rhs) {
+  return lhs.Get() == rhs.Get();
+}
+
+/**
+ * @brief Compare two optional objects lhs and rhs
+ *
+ * @param lhs Amount of VON
+ * @param rhs Amount of VON
+ * @return true if lhs > rhs, false otherwise
+ *
+ * Energon lhs(12);
+ * Energon rhs(10);
+ * if (lhs > rhs) {}
+ */
+bool operator>(const Energon& lhs, const Energon& rhs) {
+  return lhs.Get() > rhs.Get();
+}
+
+/**
+ * @brief Compare two optional objects lhs and rhs
+ *
+ * @param lhs Amount of VON
+ * @param rhs Amount of VON
+ * @return true if lhs < rhs, false otherwise
+ *
+ * Energon lhs(122);
+ * Energon rhs(12222);
+ * if (lhs < rhs) {}
+ */
+bool operator<(const Energon& lhs, const Energon& rhs) {
+  return lhs.Get() < rhs.Get();
+}
 
 /**
  * @brief Implements the addition operators for Energon
  *
- * @param a Amount of Von
- * @param b Amount of Von
+ * @param a Amount of VON
+ * @param b Amount of VON
  * @return Energon
+ *
+ * Energon energon = 1_VON + 1_LAT;
  */
-Energon operator+(const Energon& a, const Energon& b) {
-  return Energon(0).Add(a).Add(b);
+Energon operator+(const Energon& lhs, const Energon& rhs) {
+  return Energon(0).Add(lhs).Add(rhs);
+}
+
+/**
+ * @brief Implements the subtraction operator for Energon
+ *
+ * @param a Amount of VON
+ * @param b Amount of VON
+ * @return Energon
+ *
+ * Energon e = 1_kVON - 10_VON;
+ * Energon e0(10);
+ * Energon e1(2);
+ * Energon ee = e0 - e1;
+ */
+Energon operator-(const Energon& lhs, const Energon& rhs) {
+  if (lhs < rhs) {
+    platon_throw("overflow");
+  }
+  return Energon(lhs.Get() - rhs.Get());
 }
 
 /**
--- tests/unit/exchange_test.cpp
+++ tests/unit/exchange_test.cpp
@@ -4,23 +4,38 @@
 using namespace platon;
 
 TEST_CASE(exchange, energon) {
-    Energon one_von = 1_Von;
-    ASSERT(one_von.Get() == u256(VonUnit));
+    Energon one_von = 1_VON;
+    ASSERT(one_von == Energon(VON));
 
-    Energon one_gvon = 1_GVon;
-    ASSERT(one_gvon.Get() == u256(GVonUnit));
+    Energon kvon = 1_kVON;
+    ASSERT(kvon == Energon(kVON));
+
+    Energon mvon = 1_mVON;
+    ASSERT(mvon == Energon(mVON));
+
+    Energon one_gvon = 1_gVON;
+    ASSERT(one_gvon.Get() == u256(gVON));
+
+    Energon ulat = 1_uLAT;
+    ASSERT(ulat == Energon(uLAT));
+
+    Energon mlat = 1_mLAT;
+    ASSERT(mlat == Energon(mLAT));
 
     Energon one_lat = 1_LAT;
-    ASSERT(one_lat.Get() == u256(LATUnit));
+    ASSERT(one_lat.Get() == u256(LAT));
 
-    one_lat += 1_Von;
-    ASSERT(one_lat.Get() == u256(LATUnit + VonUnit));
+    one_lat += 1_VON;
+    ASSERT(one_lat.Get() == u256(LAT + VON));
 
-    Energon gon = 1_Von + 1_LAT;
-    ASSERT(gon.Get() == u256(VonUnit+LATUnit));
+    Energon gon = 1_VON + 1_LAT;
+    ASSERT(gon.Get() == u256(VON+LAT));
 
-    ASSERT((1.5_LAT).Get() == u256(1.5*LATUnit));
-    ASSERT((0.0000000122_GVon).Get() == u256(0.0000000122*GVonUnit));
+    ASSERT(one_von < kvon);
+    ASSERT(mvon > kvon);
+    ASSERT(ulat > mvon);
+    ASSERT(mlat > ulat);
+    ASSERT(one_lat > mlat);
 }
 
 UNITTEST_MAIN() {
