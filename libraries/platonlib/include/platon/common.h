512f5307ea63cc141962638412fed1667e70849b
jatel <jatelmomo@hotmail.com>
2020-03-18 14:26:33 +0800

    Change the string conversion to the compile-time computed constexpr function
    add test

--- libraries/platonlib/include/platon/common.h
+++ libraries/platonlib/include/platon/common.h
@@ -2,13 +2,13 @@
 // Created by zhou.yang on 2018/10/19.
 //
 #pragma once
+#include "panic.hpp"
+#include "vector_ref.h"
 #include <algorithm>
 #include <iterator>
 #include <string>
 #include <type_traits>
 #include <vector>
-#include "vector_ref.h"
-#include "panic.hpp"
 
 namespace platon {
 using u128 = __uint128_t;
@@ -26,10 +26,13 @@ using bytesConstRef = vector_ref<byte const>;
  * @return -1 if `_i` is not a hex character, otherwise returns the
  * corresponding decimal integer.
  */
-inline int fromHexChar(char _i) noexcept {
-  if (_i >= '0' && _i <= '9') return _i - '0';
-  if (_i >= 'a' && _i <= 'f') return _i - 'a' + 10;
-  if (_i >= 'A' && _i <= 'F') return _i - 'A' + 10;
+inline constexpr int fromHexChar(char _i) noexcept {
+  if (_i >= '0' && _i <= '9')
+    return _i - '0';
+  if (_i >= 'a' && _i <= 'f')
+    return _i - 'a' + 10;
+  if (_i >= 'A' && _i <= 'F')
+    return _i - 'A' + 10;
   return -1;
 }
 
@@ -38,7 +41,7 @@ inline int fromHexChar(char _i) noexcept {
  * @param _s Hex string.
  * @return The bytes represented by the hexadecimal string s.
  */
-inline bytes fromHex(std::string const& _s) {
+inline bytes fromHex(std::string const &_s) {
   unsigned s = (_s.size() >= 2 && _s[0] == '0' && _s[1] == 'x') ? 2 : 0;
   std::vector<uint8_t> ret;
   ret.reserve((_s.size() - s + 1) / 2);
@@ -66,8 +69,8 @@ inline bytes fromHex(std::string const& _s) {
  * @param _b A string.
  * @return Byte array of stringsByte array of strings.
  */
-inline bytes asBytes(std::string const& _b) {
-  return bytes((byte const*)_b.data(), (byte const*)(_b.data() + _b.size()));
+inline bytes asBytes(std::string const &_b) {
+  return bytes((byte const *)_b.data(), (byte const *)(_b.data() + _b.size()));
 }
 
 /**
@@ -79,12 +82,12 @@ inline bytes asBytes(std::string const& _b) {
  */
 template <class Iterator>
 inline std::string toHex(Iterator _it, Iterator _end,
-                         std::string const& _prefix) {
+                         std::string const &_prefix) {
   typedef std::iterator_traits<Iterator> traits;
   static_assert(sizeof(typename traits::value_type) == 1,
                 "toHex needs byte-sized element type");
 
-  static char const* hexdigits = "0123456789abcdef";
+  static char const *hexdigits = "0123456789abcdef";
   size_t off = _prefix.size();
   std::string hex(std::distance(_it, _end) * 2 + off, '0');
   hex.replace(0, off, _prefix);
@@ -101,8 +104,7 @@ inline std::string toHex(Iterator _it, Iterator _end,
  * @return Hex string.
  * @example toHex("A\x69") == "4169"
  */
-template <class T>
-inline std::string toHex(T const& _data) {
+template <class T> inline std::string toHex(T const &_data) {
   return toHex(_data.begin(), _data.end(), "");
 }
 
@@ -111,12 +113,11 @@ inline std::string toHex(T const& _data) {
  * @param _i Integer to be calculated.
  * @return Number of bytes.
  */
-template <class T>
-inline unsigned bytesRequired(T _i) {
+template <class T> inline unsigned bytesRequired(T _i) {
   static_assert(
       !std::numeric_limits<T>::is_signed,
-      "only unsigned types or bigint supported");  // bigint does not carry sign
-                                                   // bit on shift
+      "only unsigned types or bigint supported"); // bigint does not carry sign
+                                                  // bit on shift
   unsigned i = 0;
   for (; _i != 0; ++i, _i >>= 8) {
   }
@@ -128,12 +129,11 @@ inline unsigned bytesRequired(T _i) {
  * @param _val  An unsigned integer or bigint.
  * @param o_out Convert results.
  */
-template <class T, class Out>
-inline void toBigEndian(T _val, Out& o_out) {
+template <class T, class Out> inline void toBigEndian(T _val, Out &o_out) {
   static_assert(
       !std::numeric_limits<T>::is_signed,
-      "only unsigned types or bigint supported");  // bigint does not carry sign
-                                                   // bit on shift
+      "only unsigned types or bigint supported"); // bigint does not carry sign
+                                                  // bit on shift
   for (auto i = o_out.size(); i != 0; _val >>= 8, i--) {
     T v = _val & (T)0xff;
     o_out[i - 1] = (typename Out::value_type)(uint8_t)v;
@@ -145,16 +145,14 @@ inline void toBigEndian(T _val, Out& o_out) {
  * @param _bytes A byte array of big endian.
  * @return An integer.
  */
-template <class T, class _In>
-inline T fromBigEndian(_In const& _bytes) {
+template <class T, class _In> inline T fromBigEndian(_In const &_bytes) {
   T ret = (T)0;
   for (auto i : _bytes)
     ret = (T)((ret << 8) |
               (byte)(typename std::make_unsigned<decltype(i)>::type)i);
   return ret;
 }
-}  // namespace platon
-
+} // namespace platon
 
 /**
  * @brief Converts a string to an integer of type u128.
@@ -162,17 +160,18 @@ inline T fromBigEndian(_In const& _bytes) {
  * @param len Value string length.
  * @return an integer of type u128.
  */
-inline platon::u128 operator "" _u128 (const char *str_value, size_t len){
-  bool is_hex = false;
-  if(len > 2 && '0' == str_value[0] && 'x' == str_value[1] ){
-    is_hex = true;
+inline constexpr platon::u128 operator"" _u128(const char *str_value,
+                                               size_t len) {
+  bool is_hex = false, has_prefix = false;
+  if (len > 2 && '0' == str_value[0] && 'x' == str_value[1]) {
+    is_hex = has_prefix = true;
   } else {
-    for(int i = 0; i < len; ++i){
+    for (size_t i = 0; i < len; ++i) {
       int temp = platon::fromHexChar(str_value[i]);
-      if(-1 == temp){
-        platon::internal::platon_throw("Not a valid numeric character");
+      if (-1 == temp) {
+        return 0;
       }
-      if(temp > 9){
+      if (temp > 9) {
         is_hex = true;
         break;
       }
@@ -180,11 +179,16 @@ inline platon::u128 operator "" _u128 (const char *str_value, size_t len){
   }
 
   platon::u128 result = 0;
-  if(is_hex) {
-    platon::bytes hex_bytes = platon::fromHex(str_value);
-    result = platon::fromBigEndian<platon::u128>(hex_bytes);
-  }else {
-    for(int i = 0; i < len; ++i){
+  if (is_hex) {
+    size_t begin = 0;
+    if (has_prefix)
+      begin = 2;
+    for (size_t i = begin; i < len; ++i) {
+      int temp = platon::fromHexChar(str_value[i]);
+      result = result * 16 + temp;
+    }
+  } else {
+    for (size_t i = 0; i < len; ++i) {
       int temp = platon::fromHexChar(str_value[i]);
       result = result * 10 + temp;
     }
