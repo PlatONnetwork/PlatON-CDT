dcbaca4473a14b680645ea462b189bd555a66b53
jatel <jatelmomo@hotmail.com>
2020-04-03 15:57:23 +0800

    Place the external dependencies of LLVM in the depends directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,23 +6,22 @@ cd $( dirname "${BASH_SOURCE[0]}" )/..
 
 ROOT=`pwd`
 BUILD=$ROOT/build
-LLVM_DIR=$BUILD/clang_llvm_8.0
+LLVM_DIR=$ROOT/depends/clang_llvm_8.0
 CPUS=`nproc --all`
 
 echo $ROOT
 
-if [ -d $LLVM_DIR  ]; then
-    # rm cmake cache file
-    cd $BUILD
-    ls | grep -v "clang_llvm_8.0" | xargs rm -rf
+if [ -d $BUILD ]; then
+    rm -rf build
+fi
 
+# mkdir
+mkdir -p build && cd build
+
+if [ -d $LLVM_DIR  ]; then
     cmake .. -DLLVM_CONFIG=$LLVM_DIR/bin/llvm-config
     make -j$CPUS
 else
-    rm -rf build
-    # mkdir
-    mkdir -p build && cd build
-
     cmake ..
     make -j$CPUS
 fi
