3042b4d2fcb7b37a68ca89fda3555cc68602d11f
wanghengtao <wanghengtao@juzix.net>
2020-09-22 16:14:23 +0800

    Increase the currency unit of the testnet

--- tests/unit/exchange_test.cpp
+++ tests/unit/exchange_test.cpp
@@ -16,6 +16,31 @@ TEST_CASE(exchange, energon) {
   Energon one_gvon = 1_gVON;
   ASSERT(one_gvon.Get() == u128(EnergonUnit::gVON));
 
+#ifdef TESTNET
+  Energon microlax = 1_microLAX;
+  ASSERT(microlax == Energon(u128(EnergonUnit::microLAX)));
+
+  Energon millilax = 1_milliLAX;
+  ASSERT(millilax == Energon(u128(EnergonUnit::milliLAX)));
+
+  Energon one_lax = 1_LAX;
+  ASSERT(one_lax.Get() == u128(EnergonUnit::LAX));
+
+  Energon klax = 1_kLAX;
+  ASSERT(klax.Get() == u128(EnergonUnit::kLAX));
+
+  Energon mlax = 1_mLAX;
+  ASSERT(mlax.Get() == u128(EnergonUnit::mLAX));
+
+  Energon glax = 1_gLAX;
+  ASSERT(glax.Get() == u128(EnergonUnit::gLAX));
+
+  one_lax += 1_VON;
+  ASSERT(one_lax.Get() == u128(EnergonUnit::LAX) + u128(EnergonUnit::VON));
+
+  Energon gon = 1_VON + 1_LAX;
+  ASSERT(gon.Get() == u128(EnergonUnit::VON) + u128(EnergonUnit::LAX));
+#else
   Energon microlat = 1_microLAT;
   ASSERT(microlat == Energon(u128(EnergonUnit::microLAT)));
 
@@ -35,21 +60,30 @@ TEST_CASE(exchange, energon) {
   ASSERT(glat.Get() == u128(EnergonUnit::gLAT));
 
   one_lat += 1_VON;
-  ASSERT(one_lat.Get() ==
-         u128(EnergonUnit::LAT) + u128(EnergonUnit::VON));
+  ASSERT(one_lat.Get() == u128(EnergonUnit::LAT) + u128(EnergonUnit::VON));
 
   Energon gon = 1_VON + 1_LAT;
-  ASSERT(gon.Get() ==
-         u128(EnergonUnit::VON) + u128(EnergonUnit::LAT));
+  ASSERT(gon.Get() == u128(EnergonUnit::VON) + u128(EnergonUnit::LAT));
+#endif
 
   ASSERT(one_von < kvon);
   ASSERT(mvon > kvon);
+
+#ifdef TESTNET
+  ASSERT(microlax > mvon);
+  ASSERT(millilax > microlax);
+  ASSERT(one_lax > millilax);
+  ASSERT(klax > one_lax);
+  ASSERT(mlax > klax);
+  ASSERT(glax > mlax);
+#else
   ASSERT(microlat > mvon);
   ASSERT(millilat > microlat);
   ASSERT(one_lat > millilat);
   ASSERT(klat > one_lat);
   ASSERT(mlat > klat);
   ASSERT(glat > mlat);
+#endif
 }
 
 UNITTEST_MAIN() { RUN_TEST(exchange, energon); }
